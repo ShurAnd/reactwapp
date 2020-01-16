3abf07a569f45ad722400a3be3add7e894dc5767
AASh <AASh@mail.com>
2020-01-17 00:01:27 +0300

    initial commit

---
+++ src/main/java/com/andrey/reactwebapp/ReactwappApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.reactwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactwappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactwappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/reactwebapp/ReactwappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.reactwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactwappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
